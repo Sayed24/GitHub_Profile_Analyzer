c6f538d3726be67a2d53cc3e7bab9a990f945954
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-01-03 14:54:54 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+import { fetchUser, fetchRepos } from "./api.js";
+import { renderProfile, renderRepos } from "./ui.js";
+import { renderLanguageChart } from "./charts.js";
+
+document.getElementById("searchBtn").onclick = async () => {
+  const username = document.getElementById("usernameInput").value;
+  if (!username) return;
+
+  try {
+    const user = await fetchUser(username);
+    const repos = await fetchRepos(username);
+
+    renderProfile(user);
+    renderRepos(repos);
+    renderLanguageChart(repos);
+    saveHistory(username);
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+function saveHistory(user) {
+  let h = JSON.parse(localStorage.getItem("history")) || [];
+  h = [user, ...h.filter(u => u !== user)].slice(0, 5);
+  localStorage.setItem("history", JSON.stringify(h));
+}
 
