2fc6c7af3abc3033e778f75ce169718c9ffb4303
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-01-03 14:51:19 +0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,13 @@
+const BASE_URL = "https://api.github.com/users/";
+
+export async function fetchUser(username) {
+  const res = await fetch(`${BASE_URL}${username}`);
+  if (!res.ok) throw new Error("User not found");
+  return res.json();
+}
+
+export async function fetchRepos(username) {
+  const res = await fetch(`${BASE_URL}${username}/repos?per_page=100`);
+  return res.json();
+}
 
